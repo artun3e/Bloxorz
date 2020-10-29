8ccc8b3c846b5cd0f596fb3f52f472c19d5877e9
artun3e <artunsarioglu@sabanciuniv.edu>
2020-10-29 18:28:07 +0300

    updates on movable functions - removed vertical checks

--- bloxorz.py
+++ bloxorz.py
@@ -40,46 +40,35 @@ class Bloxorz:
         this function checks if the block is vertical and can move right
     '''
     def v_right_movable(self):
-        if self.is_vertical == True:
-            if self.curr_state[1].y + 2 < self.YSIZE and self.puzzle[self.curr_state[0].x,self.curr_state[0].y+1] != 'X' and self.puzzle[self.curr_state[1].x,self.curr_state[1].y+2] != 'X' : # block can move right
-                return True
-            else:
-                return False
-        else: # block is not vertical at all
+        if self.curr_state[1].y + 2 < self.YSIZE and self.puzzle[self.curr_state[0].x,self.curr_state[0].y+1] != 'X' and self.puzzle[self.curr_state[1].x,self.curr_state[1].y+2] != 'X' : # block can move right
+            return True
+        else:
             return False
+        
 
     '''
         this function checks if the block is vertical and can move to left
     '''
     def v_left_movable(self):
-        if self.is_vertical == True:
-            if self.curr_state[1].y-2 >= 0 and self.puzzle[self.curr_state[0].x,self.curr_state[0].y-1] != 'X' and self.puzzle[self.curr_state[1].x,self.curr_state[1].y-2] != 'X'  :
-                return True
-            else:
-                return False
+        if self.curr_state[1].y-2 >= 0 and self.puzzle[self.curr_state[0].x,self.curr_state[0].y-1] != 'X' and self.puzzle[self.curr_state[1].x,self.curr_state[1].y-2] != 'X'  :
+            return True
         else:
-            return False 
+            return False
 
 
     '''
         this function checks if the block is vertical and can move upwards
     '''
     def v_up_movable(self): 
-        if self.is_vertical == True: 
-            if self.curr_state[1].x -2 >= 0 and self.puzzle[self.curr_state[0].x-1,self.curr_state[0].y] != 'X' and self.puzzle[self.curr_state[1].x-2,self.curr_state[1].y] != 'X':
-                return True
-            else:
-                return False
+        if self.curr_state[1].x -2 >= 0 and self.puzzle[self.curr_state[0].x-1,self.curr_state[0].y] != 'X' and self.puzzle[self.curr_state[1].x-2,self.curr_state[1].y] != 'X':
+            return True
         else:
             return False
 
 
     def v_down_movable(self):
-        if self.is_vertical == True:
-            if self.curr_state[1].x+2 < self.XSIZE and self.puzzle[self.curr_state[0].x+1,self.curr_state[0].y] != 'X' and self.puzzle[self.curr_state[1].x+2,self.curr_state[1].y] != 'X' :
-                return True
-            else:
-                return False
+        if self.curr_state[1].x+2 < self.XSIZE and self.puzzle[self.curr_state[0].x+1,self.curr_state[0].y] != 'X' and self.puzzle[self.curr_state[1].x+2,self.curr_state[1].y] != 'X' :
+            return True
         else:
             return False
 
@@ -88,80 +77,69 @@ class Bloxorz:
         check if the block is horizontal and can move to right
     '''
     def h_right_movable(self):
-        if self.is_vertical == False:
-            #understand the position of the horizontal block
-            if self.curr_state[0].x == self.curr_state[1].x-1:
-                if self.curr_state[0].y+1 < self.YSIZE and self.curr_state[1].y+1 < self.YSIZE and self.puzzle[self.curr_state[0].x,self.curr_state[0].y+1] != 'X' and self.puzzle[self.curr_state[1].x, self.curr_state[1].y+1] != 'X' :
-                    return True
-                else :
-                    return False
-            #no need to check the position but for the sake of readability, let's write
-            elif self.curr_state[0].y == self.curr_state[1].y-1 :
-                if self.curr_state[0].y+2 < self.YSIZE and self.curr_state[1].y+1 < self.YSIZE and self.puzzle[self.curr_state[0].x,self.curr_state[0].y+2] != 'X' and self.puzzle[self.curr_state[1].x, self.curr_state[1].y+1] != 'X' :
-                    return True
-                else:
-                    return False
-        else:
-            return False
+        #understand the position of the horizontal block
+        if self.curr_state[0].x == self.curr_state[1].x-1:
+            if self.curr_state[0].y+1 < self.YSIZE and self.curr_state[1].y+1 < self.YSIZE and self.puzzle[self.curr_state[0].x,self.curr_state[0].y+1] != 'X' and self.puzzle[self.curr_state[1].x, self.curr_state[1].y+1] != 'X' :
+                return True
+            else :
+                return False
+        #no need to check the position but for the sake of readability, let's write
+        elif self.curr_state[0].y == self.curr_state[1].y-1 :
+            if self.curr_state[0].y+2 < self.YSIZE and self.curr_state[1].y+1 < self.YSIZE and self.puzzle[self.curr_state[0].x,self.curr_state[0].y+2] != 'X' and self.puzzle[self.curr_state[1].x, self.curr_state[1].y+1] != 'X' :
+                return True
+            else:
+                return False
 
     
     '''
         check if the block is horizontal and can move to left
     '''
     def h_left_movable(self):
-        if self.is_vertical == False:
-            if self.curr_state[0].x == self.curr_state[1].x-1: # means y coordinates are constant
-                if self.curr_state[0].y-1 >= 0 and self.curr_state[1].y-1 >= 0 and self.puzzle[self.curr_state[0].x,self.curr_state[0].y-1] != 'X' and self.puzzle[self.curr_state[1].x,self.curr_state[1].y-1] != 'X' :
-                    return True
-                else :
-                    return False
-            
-            elif self.curr_state[0].y == self.curr_state[1].y-1 : # means x coordinates are constant
-                if self.curr_state[0].y-1 >= 0 and self.curr_state[1].y-2 >= 0 and self.puzzle[self.curr_state[0].x,self.curr_state[0].y-1] != 'X' and self.puzzle[self.curr_state[1].x,self.curr_state[1].y-2] != 'X' :
-                    return True
-                else : 
-                    return False
-        else:
-            return False
+        if self.curr_state[0].x == self.curr_state[1].x-1: # means y coordinates are constant
+            if self.curr_state[0].y-1 >= 0 and self.curr_state[1].y-1 >= 0 and self.puzzle[self.curr_state[0].x,self.curr_state[0].y-1] != 'X' and self.puzzle[self.curr_state[1].x,self.curr_state[1].y-1] != 'X' :
+                return True
+            else :
+                return False
+        
+        elif self.curr_state[0].y == self.curr_state[1].y-1 : # means x coordinates are constant
+            if self.curr_state[0].y-1 >= 0 and self.curr_state[1].y-2 >= 0 and self.puzzle[self.curr_state[0].x,self.curr_state[0].y-1] != 'X' and self.puzzle[self.curr_state[1].x,self.curr_state[1].y-2] != 'X' :
+                return True
+            else : 
+                return False
 
     '''
         check if the block is horizontal and can move upwards
     '''
     def h_up_movable(self):
-        if self.is_vertical == False:
-            if self.curr_state[0].x == self.curr_state[1].x :
-                if self.curr_state[0].x-1 >= 0 and self.curr_state[1].x-1 >= 0 and self.puzzle[self.curr_state[0].x-1,self.curr_state[0].y] != 'X' and self.puzzle[self.curr_state[1].x-1,self.curr_state[1].y] != 'X' :
-                    return True
-                else : 
-                    return False
+        if self.curr_state[0].x == self.curr_state[1].x :
+            if self.curr_state[0].x-1 >= 0 and self.curr_state[1].x-1 >= 0 and self.puzzle[self.curr_state[0].x-1,self.curr_state[0].y] != 'X' and self.puzzle[self.curr_state[1].x-1,self.curr_state[1].y] != 'X' :
+                return True
+            else : 
+                return False
 
-            elif self.curr_state[0].y  == self.curr_state[1].y :
-                if self.curr_state[0].x-1 >= 0 and self.curr_state[1].x-2 >= 0 and self.puzzle[self.curr_state[0].x-1,self.curr_state[0].y] != 'X' and self.puzzle[self.curr_state[1].x-2,self.curr_state[1].y] != 'X' : 
-                    return True
-                else :
-                    return False
-        else:
-            return False 
-
+        elif self.curr_state[0].y  == self.curr_state[1].y :
+            if self.curr_state[0].x-1 >= 0 and self.curr_state[1].x-2 >= 0 and self.puzzle[self.curr_state[0].x-1,self.curr_state[0].y] != 'X' and self.puzzle[self.curr_state[1].x-2,self.curr_state[1].y] != 'X' : 
+                return True
+            else :
+                return False
+       
         
     '''
         check if the block is horizontal and can move downwards 
     '''
     def h_down_movable(self):
-        if self.is_vertical == False:
-            if self.curr_state[0].x == self.curr_state[1].x :
-                if self.curr_state[0].x+1 < self.XSIZE and self.curr_state[1].x+1 < self.XSIZE and self.puzzle[self.curr_state[0].x+1,self.curr_state[0].y] != 'X' and self.puzzle[self.curr_state[1].x+1,self.curr_state[1].y] != 'X' :
-                    return True
-                else : 
-                    return False
+        if self.curr_state[0].x == self.curr_state[1].x :
+            if self.curr_state[0].x+1 < self.XSIZE and self.curr_state[1].x+1 < self.XSIZE and self.puzzle[self.curr_state[0].x+1,self.curr_state[0].y] != 'X' and self.puzzle[self.curr_state[1].x+1,self.curr_state[1].y] != 'X' :
+                return True
+            else : 
+                return False
 
-            elif self.curr_state[0].y  == self.curr_state[1].y :
-                if self.curr_state[0].x+2 < self.XSIZE and self.curr_state[1].x+1 < self.XSIZE and self.puzzle[self.curr_state[0].x+2,self.curr_state[0].y] != 'X' and self.puzzle[self.curr_state[1].x+1,self.curr_state[1].y] != 'X' : 
-                    return True
-                else :
-                    return False
-        else:
-            return False 
+        elif self.curr_state[0].y  == self.curr_state[1].y :
+            if self.curr_state[0].x+2 < self.XSIZE and self.curr_state[1].x+1 < self.XSIZE and self.puzzle[self.curr_state[0].x+2,self.curr_state[0].y] != 'X' and self.puzzle[self.curr_state[1].x+1,self.curr_state[1].y] != 'X' : 
+                return True
+            else :
+                return False
+     
 
 
 
@@ -177,6 +155,18 @@ class Bloxorz:
 
 
     
+    '''
+        function to move right 
+        Two possible states for moving the blocks:
+        right-left
+    '''
+    def move_right(self): 
+
+        if self.is_vertical :  # 
+
+        else :
+
+
 
     #Uniform Search Cost Search algorithm for solving the puzzle
     '''
