206196afd36b31258a2ce045a4570f9e63a94f5d
artun3e <artunsarioglu@sabanciuniv.edu>
2020-11-05 19:36:32 +0300

    create matrix function

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 import bloxorz
+
+
+def create_matrix():
+    puzzle = []
+    puzzle_file = open("input_matrix.txt")
+    for line in puzzle_file:
+        puzzle.append(line.split())
+
+    return puzzle
+
+def main() :
+    puzzle = create_matrix()
+
+
+if __name__ == "__main__" : 
+    main()
